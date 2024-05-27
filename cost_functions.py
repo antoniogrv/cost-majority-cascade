3e71d97ac71d11ea2fbd13e03b21aa163d43f20b
DarioTroll <puddino1337@gmail.com>
2024-05-27 10:49:57 +0200

    Added a third cost function

--- cost_functions.py
+++ cost_functions.py
@@ -1,16 +1,23 @@
 import math
 import random
 
-from networkx import Graph
-
-# Valori minimi e massimi per il range utilizzato dalla prima funzione di costo
+#Valori minimi e massimi per il range utilizzato dalla prima funzione di costo
 range_min = 1
 range_max = 10
 
+maxDegree = 0
+
 cost_map = {}
 
-# Questa funzione va invocata una sola volta, prima di ogni esperimento in cui la funzione di costo scelta è la prima funzione di costo
-def initialize_cost_map(G: Graph):
+isDmaxSet = False
+dmax = 0
+
+def setDmax(G):
+    global dmax
+    dmax = max(dict(G.degree()).values())
+
+#Questa funzione va invocata una sola volta, prima di ogni esperimento in cui la funzione di costo scelta è la prima funzione di costo
+def initialize_cost_map(G):
     for node in list(G.nodes()):
         cost_map[node] = random.randint(range_min, range_max)
 
@@ -18,28 +25,32 @@ def print_cost_map():
     for node, cost in cost_map.items():
         print(f"Nodo: {node}, Costo: {cost}")
     
-def first(G: Graph, v):
-    if isinstance(v, list):
-        costSum = 0
 
+def first(G,v):
+    if isinstance(v, list):
+
+        costSum = 0
         for u in v:
-            costSum += cost_map[u]
+            costSum+=cost_map[u]
         return costSum
     else:
         return cost_map[v]
 
-def second(G: Graph, v):
+def second(G,v):
     if isinstance(v, list):
         costSum = 0
-
         for u in v:
-            costSum += math.ceil(G.degree(u)/2)
+            costSum+=math.ceil(G.degree(u)/2)
         return costSum
     else:
         return math.ceil(G.degree(v)/2)
 
-'''
-#To be defined
 def third(G,v):
-    return ...;
-'''
+    if not isDmaxSet: setDmax(G)
+    if isinstance(v, list):
+        costSum = 0
+        for u in v:
+            costSum+=math.floor((G.degree(u)**2)/dmax)
+        return costSum
+    else:
+        return math.floor((G.degree(v)**2)/dmax)
